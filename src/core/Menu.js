faf640682282103729783136cd587eb4d1b01826
pratham534 <prathamsrivastava534@gmail.com>
2023-06-09 23:59:41 +0530

    some css changes

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -21,15 +21,15 @@ export default function Menu(history, path) {
                     <Link className="navbar-brand navbar-text text-black" to='/'>Shopical</Link>
                     <div className="collapse navbar-collapse gap-2" id="navbarSupportedContent">
                         <ul className="navbar-nav mb-lg-0 d-flex">
-                            {/* <li className="nav-item">
+                            <li className="nav-item">
                                 <Link className="nav-link" to='/'>Categories</Link>
-                            </li> */}
+                            </li>
                         </ul>
                         <form className="d-flex w-100 justify-content-between gap-2" role="search">
-                            {/* <div className='w-50 d-flex gap-2'>
+                            <div className='w-50 d-flex gap-2'>
                                 <input className="form-control w-100" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-dark" type="submit">Search</button>
-                            </div> */}
+                            </div>
                             <div className='w-auto d-flex gap-2'>
                                 {(!isAuthenticated()) && <Link to='/signin'><button className="btn btn-outline-dark" type="submit">Login</button></Link>}
                                 {(isAuthenticated()) && <button className="btn btn-outline-dark" onClick={handleCart}>Cart</button>}
